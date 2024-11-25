43956bf12205f1a1cb68fc684e900cc46fefdd01
noam tamari <noamtamari98@gmail.com>
2024-11-25 15:06:45 +0200

    FacilityType constructor and functions

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -1,4 +1,32 @@
 #include "Facility.h"
 
-// Cons
-FacilityType::FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score) : name(name), category(category), price(price), lifeQuality_score(lifeQuality_score), economy_score(economy_score), environment_score(environment_score) {}
+// Constructor
+FacilityType::FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score)
+    : name(name), category(category), price(price), lifeQuality_score(lifeQuality_score), economy_score(economy_score), environment_score(environment_score) {}
+
+// returning the name of the facility
+const string &FacilityType::getName() const
+{
+    return name;
+}
+
+int FacilityType::getCost() const
+{
+    return price;
+}
+
+int FacilityType::getLifeQualityScore() const{
+    return lifeQuality_score;
+}
+
+int FacilityType::getEnvironmentScore() const{
+    return environment_score;
+}
+
+int FacilityType::getEconomyScore() const{
+    return economy_score;
+}
+
+FacilityCategory FacilityType::getCategory() const{
+    return category;
+}
