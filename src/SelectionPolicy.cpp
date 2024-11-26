7bcc8a34b7d72d8e6fea1fcd9e203788b2f5a56a
Noy Segal <n3segal@gmail.com>
2024-11-26 09:55:34 +0200

    Start selection policy

---
+++ src/SelectionPolicy.cpp
@@ -0,0 +1,19 @@
+
+#include "SelectionPolicy.h"
+
+
+NaiveSelection::NaiveSelection(): lastSelectedIndex(-1){}
+
+// Assume facilities Options is not empty 
+const FacilityType& NaiveSelection::selectFacility(const vector<FacilityType>& facilitiesOptions) {
+    lastSelectedIndex = (lastSelectedIndex + 1) % facilitiesOptions.size();
+    return facilitiesOptions[lastSelectedIndex];
+}
+
+// to fill
+const string NaiveSelection::toString() const {
+
+}
+
+NaiveSelection *clone() const override;
+~NaiveSelection() override = default;
